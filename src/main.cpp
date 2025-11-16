fd869a7d2d1e05004b4a930b69dfa06224c34961
Stefan Ciutina <iJynx@protonmail.com>
2025-11-16 18:29:51 +0800

    feat: exit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
+#include <functional>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
+
+std::unordered_map<std::string, std::function<int(const std::vector<std::string> &)>> commands;
+
+int cmd_exit(const std::vector<std::string> &args) {
+    std::exit(0);
+    return 0;
+}
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  while (true) {
+    commands = {{"exit", cmd_exit}};
 
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
+    while (true) {
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
 
-    std::cout << input << ": command not found\n";
-  }
+        std::vector<std::string> args;
+
+        int idx = 0;
+        int count = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+            if (input[i] == ' ') {
+                if (count > 0)
+                    args.push_back(input.substr(idx, count));
+
+                idx = i + 1;
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+        if (count > 0)
+            args.push_back(input.substr(idx, count));
+
+        if (commands.count(args[0]) == 0) {
+            std::cout << input << ": command not found\n";
+        } else {
+            commands[args[0]](args);
+        }
+    }
 }
