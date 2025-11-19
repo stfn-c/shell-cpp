12bf618bab8bf7afe5a93404974b383cc5652037
Stefan Ciutina <iJynx@protonmail.com>
2025-11-19 12:59:33 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,46 @@
 
 ShellState shell_state;
 
+enum class QuoteState { NONE, SINGLE, DOUBLE, ESCAPE };
+
 std::vector<std::string> parse_args(const std::string &input) {
     std::vector<std::string> args;
     std::string current_arg;
-    bool in_quotes = false;
+    QuoteState quote_state = QuoteState::NONE;
 
     for (char c : input) {
-        if (c == '\'' && in_quotes) {
-            in_quotes = false;
-        } else if (c == '\'' && !in_quotes) {
-            in_quotes = true;
-        } else if (!in_quotes && c == ' ') {
-            if (!current_arg.empty()) {
-                args.push_back(current_arg);
-                current_arg = "";
+        switch (quote_state) {
+        case QuoteState::NONE:
+            if (c == '\'') {
+                quote_state = QuoteState::SINGLE;
+            } else if (c == '\"') {
+                quote_state = QuoteState::DOUBLE;
+            } else if (c == ' ') {
+                if (!current_arg.empty()) {
+                    args.push_back(current_arg);
+                    current_arg.clear();
+                }
+            } else {
+                current_arg += c;
             }
-        } else {
-            current_arg += c;
+            break;
+        case QuoteState::SINGLE:
+            if (c == '\'') {
+                quote_state = QuoteState::NONE;
+            } else {
+                current_arg += c;
+            }
+            break;
+        case QuoteState::DOUBLE:
+            if (c == '\"') {
+                quote_state = QuoteState::NONE;
+            } else {
+                current_arg += c;
+            }
+            break;
+        case QuoteState::ESCAPE:
+            std::cout << "not handled yet\n";
+            break;
         }
     }
 
